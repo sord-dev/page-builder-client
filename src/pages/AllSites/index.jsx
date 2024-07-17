b4a43c6c7fd4dfcf8242c0670df5e6cd660bd3fc
MisterCxmpy <cosgunhussein@gmail.com>
2024-07-17 22:59:37 +0100

    unsafe

--- src/pages/AllSites/index.jsx
+++ src/pages/AllSites/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import lodash from 'lodash'
 import { useAppContext } from '../../context/appContext'
 
-const components = [{ type: 'ContactForm' }, { type: 'HeadingWith4IconsAndArrows', props: { title: 'string', icon: [] } }, { type: 'HeadingWith6Icons', props: { title: 'string', icon: [] } }] // temporary data, will be from a context or api
+const components = [{ type: 'ContactForm', props: {} }, { type: 'HeadingWith4IconsAndArrows', props: { title: 'string', icon: [] } }, { type: 'HeadingWith6Icons', props: { title: 'string', icon: [] } }] // temporary data, will be from a context or api
 
 function AllSites() {
     const { appState } = useAppContext()
@@ -11,11 +11,19 @@ function AllSites() {
         return btoa(JSON.stringify(data))
     }
 
-    const buildFakePage = (component, userValues) => {
-        const componentData = lodash.cloneDeep(component)
-        componentData.props = lodash.merge(componentData.props, userValues)
+    const buildFakePage = (components, userValues) => {
 
-        return { components: [componentData] };
+        let processComponent = components.map((c, i) => {
+            const componentData = lodash.cloneDeep(c)
+            componentData.props = lodash.merge(componentData.props, userValues)
+
+            
+            return componentData
+        })
+        
+        console.log(processComponent)
+
+        return { components: processComponent };
     }
 
     return (
@@ -26,7 +34,7 @@ function AllSites() {
             <ul>
                 {components?.map((component, index) => (
                     <li key={index}>
-                        <a href={`/sites/render?data=${encodeData(buildFakePage(component, {
+                        <a href={`/sites/render?data=${encodeData(buildFakePage([component], {
                             title: 'test title', icon: [{
                                 icon: null,
                                 title: "Title goes here",
@@ -35,6 +43,13 @@ function AllSites() {
                         }))}`}>{component.type}</a>
                     </li>
                 ))}
+                <a href={`/sites/render?data=${encodeData(buildFakePage([components[0], components[1], components[2]], {
+                            title: 'test title', icon: [{
+                                icon: null,
+                                title: "Title goes here",
+                                desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta neque, officia rerum cum voluptatum asperiores quos! Dolor, ipsa ea eos laborum laboriosam doloribus voluptate quisquam sequi molestiae temporibus officia eveniet?",
+                            }]
+                        }))}`}>bomba</a>
             </ul>
         </div>
     )
